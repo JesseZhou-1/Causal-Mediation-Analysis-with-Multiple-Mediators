d2471d552bff0207378f4b4b9c85dcc1c8ec9c05
Jesse Zhou <69114052+JesseZhou-1@users.noreply.github.com>
2024-04-06 00:35:32 -0500

    Update natl_2003.py

--- natl_2003.py
+++ natl_2003.py
@@ -21,6 +21,7 @@ dataset_name = 'cleaned_natl2003_bin'  # Define the name of the dataset.
 if not (os.path.isdir(path)):  # checks if a directory with the name 'path' exists.
     os.makedirs(path)  # if not, creates a new directory with this name. This is where the logs and model weights will be saved.
 
+## MODEL TRAINING
 from trainMed import trainMed
 trainMed(path=path, dataset_name=dataset_name, treatment='a', confounder=["c1", "c2", "c3", "c4"], mediator=["l", "m"], outcome='y',
            test_size=0.2, cat_var=["a", "l", "m", "y", "c1", "c2", "c3", "c4"], sens_corr=None, seed_split=1,
@@ -30,10 +31,14 @@ trainMed(path=path, dataset_name=dataset_name, treatment='a', confounder=["c1", 
            emb_net=[100, 90, 80, 70, 60],
            int_net=[60, 50, 40, 30, 20])
 
+## EFFECT ESTIMATION
 from simMed import simMed
+
+# Path-specific effects
 simMed(path=path, dataset_name=dataset_name, model_name=path + 'seed_1', n_mce_samples=1000000, seed=1, inv_datafile_name='1_path_1M',
         cat_list=[0, 1], moderator=None)
 
+# Interventional effects
 simMed(path=path, dataset_name=dataset_name, model_name=path + 'seed_1', n_mce_samples=1000000, mediator=["m=intv"], seed=1, inv_datafile_name='1_intv_1M',
         cat_list=[0, 1], moderator=None)
 
